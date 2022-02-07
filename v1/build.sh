e8947d20e7a03112765f33b81759b028df30361a
andy <andreas.olaus@gmail.com>
2022-02-07 14:38:46 +0100

    SWEET ALT TAB!

--- v1/build.sh
+++ v1/build.sh
@@ -1,7 +1,9 @@
 qmk json2c restructure.json -o keymap.c
 src=$(pwd)
+cat keymap.c | head -n 8 > kmhead.c
+cat keymap.c | tail -n 8 > kmtail.c
 cd  ~/_code/qmk/qmk_firmware/keyboards/keycapsss/kimiko/keymaps/mymap/
-cat "$src/keymap.c" combos.mc > keymap.c
+cat "$src/kmhead.c" combos.mc "$src/kmtail.c" > keymap.c
 
 qmk compile
 mv ~/_code/qmk/qmk_firmware/keycapsss_kimiko_rev1_mymap.hex $src
