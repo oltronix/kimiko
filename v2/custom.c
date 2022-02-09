500bc56488fda0d323576a7fc4c693e5ecebfc52
andy <andreas.olaus@gmail.com>
2022-02-09 22:08:52 +0100

    using the oneshot mods from callum

--- v2/custom.c
+++ v2/custom.c
@@ -2,8 +2,15 @@
 
 #include "oneshot.h"
 
-#define L_LEFT MO(1)
-#define L_RIGHT MO(2)
+#define L_MODL MO(1)
+#define R_MODL MO(2)
+
+#define R_SYML MO(4) //Toggle symbols on left hand
+#define R_NAV MO(6) //Toggle mouse layer for left
+#define L_SYML MO(5) //Toggle symbols on right hand
+#define L_NUMP MO(3) //Toggle num pad on right hand
+#define L_NAV MO(7)  //Toggle navigation on right hand
+
 /* OVERRIDES */
 
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
@@ -24,7 +31,7 @@ enum custom_keycodes {          // Make sure have the awesome keycode ready
     OS_SHFT,
     OS_CTRL,
     OS_ALT,
-    OS_CMD
+    OS_GUI
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -42,7 +49,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 oneshot_state os_shft_state = os_up_unqueued;
 oneshot_state os_ctrl_state = os_up_unqueued;
 oneshot_state os_alt_state = os_up_unqueued;
-oneshot_state os_cmd_state = os_up_unqueued;
+oneshot_state os_gui_state = os_up_unqueued;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     update_oneshot(
@@ -58,7 +65,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         keycode, record
     );
     update_oneshot(
-        &os_cmd_state, KC_LCMD, OS_CMD,
+        &os_gui_state, KC_LEFT_GUI, OS_GUI,
         keycode, record
     );
   switch (keycode){
@@ -98,8 +105,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool is_oneshot_cancel_key(uint16_t keycode) {
     switch (keycode) {
-    case L_LEFT:
-    case L_RIGHT:
+    case KC_ESC:
         return true;
     default:
         return false;
@@ -108,13 +114,18 @@ bool is_oneshot_cancel_key(uint16_t keycode) {
 
 bool is_oneshot_ignored_key(uint16_t keycode) {
     switch (keycode) {
-    case L_LEFT:
-    case L_RIGHT:
+    case L_MODL:
+    case R_MODL:
     case KC_LSFT:
     case OS_SHFT:
     case OS_CTRL:
     case OS_ALT:
-    case OS_CMD:
+    case OS_GUI:
+    case R_SYML:
+    case R_NAV:
+    case L_SYML:
+    case L_NUMP:
+    case L_NAV:
         return true;
     default:
         return false;
