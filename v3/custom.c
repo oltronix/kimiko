e5122ba342616ec6221ad610bcf898b2ef08ac53
andreas.o <andreas.olausson@nelly.com>
2022-03-06 09:15:59 +0100

    Fixed issue with alt enter, shift delete

--- v3/custom.c
+++ v3/custom.c
@@ -60,13 +60,13 @@ combo_t key_combos[COMBO_LENGTH] = {
 
 /* OVERRIDES */
 
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+///const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override,
-    NULL // Null terminate the array of overrides!
-};
+//const key_override_t **key_overrides = (const key_override_t *[]){
+//    &delete_key_override,
+//    NULL // Null terminate the array of overrides!
+//};
 
 
 bool is_alt_tab_active = false;
