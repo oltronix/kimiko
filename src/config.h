f339bc0167a2521636281e9c7a27321be2131041
andreas o <andreas.olausson@outnordic.com>
2026-01-25 00:00:10 +0100

    fix oled

--- src/config.h
+++ src/config.h
@@ -31,7 +31,7 @@
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define OLED_FONT_H "glcdfont.c"
+#define OLED_FONT_H "keyboards/keycapsss/kimiko/rev2/glcdfont.c"
 //#define OLED_FONT_WIDTH 5
 //#define OLED_FONT_HEIGHT 7
 
